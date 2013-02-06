2dca39a3e2691a47a8721f5f263d8f51eecb636a
Christopher Fu <chrisf1337@gmail.com>
2013-02-06 14:26:16 -0800

    Added input method

--- JavaChat/src/JavaChatHandler.java
+++ JavaChat/src/JavaChatHandler.java
@@ -1,8 +1,28 @@
+import java.io.*;
 
 public class JavaChatHandler
 {
     public JavaChatHandler()
     {
         
+    }
+    
+    public void input()
+    {
+        System.out.println("Enter your name:");
+        BufferedReader br = 
+                new BufferedReader(new InputStreamReader(System.in));
+        String username = null;
+        try 
+        {
+            username = br.readLine();
+        } 
+        catch(IOException e)
+        {
+            System.out.println("Error reading name");
+            e.printStackTrace();
+        }
+        
+        System.out.println("Your name is " + username);
     }
 }
